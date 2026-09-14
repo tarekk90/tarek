bbcc1f35796b27dcc5f4abbf52437bef768e3801
tarekk90 <tarekbenchikh444@gmail.com>
2026-09-14 09:26:08 +0000

    task 2

--- MyFirstSApp/Program.cs
+++ MyFirstSApp/Program.cs
@@ -4,11 +4,18 @@ class Program
 {
     static void Main()
     {
+        // Заголовок программы
         Console.WriteLine("========================================");
         Console.WriteLine("       CHARACTER GENERATOR              ");
+        
+        // Информация об авторе
         Console.WriteLine("       Автор: Тарек                     ");
+        
+        // Название лабораторной работы
         Console.WriteLine("       Лабораторная работа №1           ");
         Console.WriteLine("========================================");
+        
+        // Приветствие пользователя
         Console.WriteLine("Добро пожаловать в генератор!");
         Console.WriteLine("========================================");
     }
