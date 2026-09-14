cdad1c26aa0d7b3543e531f808cf7b8a66777b73
tarekk90 <tarekbenchikh444@gmail.com>
2026-09-14 08:47:41 +0000

    Ex 1

---
+++ MyFirstSApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("========================================");
+        Console.WriteLine("       CHARACTER GENERATOR              ");
+        Console.WriteLine("       Автор: Тарек                     ");
+        Console.WriteLine("       Лабораторная работа №1           ");
+        Console.WriteLine("========================================");
+        Console.WriteLine("Добро пожаловать в генератор!");
+        Console.WriteLine("========================================");
+    }
+}
